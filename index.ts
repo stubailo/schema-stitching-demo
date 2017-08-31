3c30719d9c73ff7c8c4b59b99f9a5f80e5488f8d
Sashko Stubailo <sashko@meteor.com>
2017-08-31 12:12:23 -0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,70 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeRemoteExecutableSchema, mergeSchemas } from 'graphql-tools';
+import { createApolloFetch } from 'apollo-fetch';
+
+async function run() {
+  // XXX fix typings
+  const universeSchema = await makeRemoteExecutableSchema(createApolloFetch({
+    uri: 'https://www.universe.com/graphql/beta',
+  }) as any);
+
+  const weatherSchema = await makeRemoteExecutableSchema(createApolloFetch({
+    uri: 'https://5rrx10z19.lp.gql.zone/graphql',
+  }) as any);
+
+  const schema = mergeSchemas({
+    schemas: [universeSchema, weatherSchema],
+    links: [
+      {
+        name: 'location',
+        from: 'Event',
+        to: 'location',
+        resolveArgs: parent => ({ place: parent.cityName }),
+        fragment: `
+          fragment WeatherLocationArgs on Event {
+            cityName
+          }
+        `,
+      },
+    ],
+  });
+
+  const app = express();
+
+  app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+  app.use(
+    '/graphiql',
+    graphiqlExpress({
+      endpointURL: '/graphql',
+      query: `
+        query {
+          event(id: "5983706debf3140039d1e8b4") {
+            title
+            description
+            url
+            location {
+              city
+              country
+              weather {
+                summary
+                temperature
+              }
+            }
+          }
+        }
+      `,
+    })
+  );
+
+  app.listen(3000);
+  console.log('Listening!');
+}
+
+try {
+  run();
+} catch (e) {
+  console.log(e, e.message, e.stack);
+}
